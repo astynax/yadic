bef76f7ee86e46cf499b11cfdcccd2020151d8ee
Aleksey Pirogov <astynax@users.noreply.github.com>
2015-03-07 07:35:26 +0300

    0.1.12.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="yadic",
-    version="0.1.12.2",
+    version="0.1.12.3",
     license='MIT',
     description=read('DESCRIPTION'),
     author="Aleksey Pirogov",
